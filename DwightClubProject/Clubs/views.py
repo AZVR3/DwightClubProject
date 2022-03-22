4bfd05d5cea60a49b15ca14d139ccc5e41b4a915
AZVR3 <jongyul3213@gmail.com>
2022-03-22 13:29:20 +0900

    Restart of django project

--- DwightClubProject/Clubs/views.py
+++
@@ -1,7 +0,0 @@
-from curses.ascii import HT
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def calendar(request):
-    return HttpResponse("Hello, you are at the calendar page.")
