a8eaf9b9dd5355cc2d10a7086607372f978b66af
AZVR3 <jongyul3213@gmail.com>
2022-04-05 16:30:54 +0900

    work in progress

--- DwightClubProject/clubs/urls.py
+++ DwightClubProject/clubs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("/feed", views.feed, name="feed"),
+    path("/club", views.club, name="clubs")
 ]
--- DwightClubProject/clubs/views.py
+++ DwightClubProject/clubs/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from clubs.forms import UserForm
+
+clubs = [
+    {'clubname': 'entrepeneurship'},
+    {''}
+]
+
 # Create your views here.
 def home(request):
-    context = {"name": "student"}
+    context = {"name": "Student", "clubs": clubs}
     return render(request, "clubs/home.html", context)
+
+def feed(request):
+    context = {"name": "Student", "clubs": clubs}
+    return render(request, "clubs/feed.html", context)
+
+def club(request):
+    context = {"name": "Student", "clubs": clubs}
+    return render(request, "clubs/club.html", context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return rediret('home')
+    else:
+        form = UserForm()
+
+    return render(request, 'register.html', {'form':form})
+    
