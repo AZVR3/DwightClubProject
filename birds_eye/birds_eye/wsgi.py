ae56929a0ea55dd9a5d9daa88150c11847a6b182
AZVR3 <jongyul3213@gmail.com>
2022-05-08 05:59:48 +0900

    .

--- birds_eye/birds_eye/wsgi.py
+++ birds_eye/birds_eye/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BirdsEye project.
+WSGI config for birds_eye project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BirdsEye.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birds_eye.settings')
 
 application = get_wsgi_application()
