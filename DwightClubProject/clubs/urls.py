0127592ff2f32954bc79e3e7ca3ac4d7207cd981
AZVR3 <jongyul3213@gmail.com>
2022-05-08 05:36:13 +0900

    restart

--- DwightClubProject/clubs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("/feed", views.feed, name="feed"),
-    path("/club", views.club, name="clubs")
-]
